a32b2a6b80273b75ea63f2d4c6efdc6b6f19a58c
Volodymyr Taranin <crm.c.30187@gmail.com>
2025-03-08 11:38:36 +0200

    add form error message

--- lib/action.ts
+++ lib/action.ts
@@ -7,7 +7,7 @@ function isInvalidText(text: string) {
   return !text || text.trim() === "";
 }
 
-export async function shareMeal(formData: FormData) {
+export async function shareMeal(prevState: any, formData: FormData) {
   const meal = {
     title: formData.get("title"),
     summary: formData.get("summary"),
@@ -27,7 +27,9 @@ export async function shareMeal(formData: FormData) {
     !meal.image ||
     (meal.image as File).size === 0
   ) {
-    throw new Error("Invalid input");
+    return {
+      message: "Invalid input.",
+    };
   }
 
   await saveMeal(meal);
--- src/app/meals/share/page.tsx
+++ src/app/meals/share/page.tsx
@@ -1,9 +1,13 @@
+"use client";
+
+import { useActionState } from "react";
 import ImagePicker from "@/components/meals/image-picker";
 import classes from "./share.module.css";
 import { shareMeal } from "@/../lib/action";
 import MealsFormSubmit from "@/components/meals/meals-form-submit";
 
 export default function ShareMealPage() {
+  const [state, formAction] = useActionState(shareMeal, { message: "" });
 
   return (
     <>
@@ -14,7 +18,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form} action={shareMeal}>
+        <form className={classes.form} action={formAction}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -43,6 +47,7 @@ export default function ShareMealPage() {
             ></textarea>
           </p>
           <ImagePicker label="Your image" name="image" />
+          {state.message && <p>{state.message}</p>}
           <p className={classes.actions}>
             <MealsFormSubmit />
           </p>
