09e983400a5385d53987326b882efbf8d0178f6b
Volodymyr Taranin <crm.c.30187@gmail.com>
2025-03-08 10:44:31 +0200

    add save meal

--- lib/meals.ts
+++ lib/meals.ts
@@ -1,4 +1,7 @@
+import fs from "node:fs";
 import sql from "better-sqlite3";
+import slugify from "slugify";
+import xss from "xss";
 
 const db = sql("meals.db");
 
@@ -10,4 +13,37 @@ export async function getMeals() {
 export async function getMeal(slug: string) {
   await new Promise((resolve) => setTimeout(resolve, 100));
   return db.prepare("SELECT * FROM meals WHERE slug = ?").get(slug);
+}
+
+export async function saveMeal(meal: any) {
+  meal.slug = slugify(meal.title, { lower: true });
+  meal.instructions = xss(meal.instructions);
+
+  const extension = meal.image.name.split(".").pop();
+  const fileName = `${meal.slug}.${extension}`;
+  const stream = fs.createWriteStream(`public/images/${fileName}`);
+  const bufferedImage = await meal.image.arrayBuffer();
+
+  stream.write(Buffer.from(bufferedImage), (error) => {
+    if (error) {
+      throw new Error("Saving image failed!");
+    }
+  });
+
+  meal.image = `/images/${fileName}`;
+  db.prepare(
+    `
+      INSERT INTO meals
+        (title, summary, instructions, creator, creator_email, image, slug)
+      VALUES (
+        @title,
+        @summary,
+        @instructions,
+        @creator,
+        @creator_email,
+        @image,
+        @slug
+      )
+    `
+  ).run(meal);
 }
---
+++ src/types/meal.ts
@@ -0,0 +1,10 @@
+export interface Meal {
+  id: number;
+  slug: string;
+  title: string;
+  image: string;
+  summary: string;
+  instructions: string;
+  creator: string;
+  creator_email: string;
+}
